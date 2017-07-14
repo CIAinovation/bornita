dd2ef60aacb4ea40882b2c9fce55b54ec71357f1
ivan alfonso villa vazquez <drg_henri@hotmail.com>
2017-07-13 20:34:06 -0500

    v1

---
+++ index.php
@@ -0,0 +1,339 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <title>Parallax Template - Materialize</title>
+
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link href="materialize/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="materialize/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="materialize/css/main.css" type="text/css" rel="stylesheet" />
+    <link rel="stylesheet" href="materialize/css/font-awesome-4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+
+<!--          Menu         -->
+<nav class="nav-extended white top-menu" id="top-menu">
+    <div class="nav-wrapper " >
+      <a href="#" class="brand-logo">Logo</a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li class="selected"><a class="" >Nosotros</a></li>
+        <li ><a class="" href="src/jewelry.php">Joyeria</a></li>
+        <li><a class="" href="collapsible.html">Hogar</a></li>
+        <li><a class="" href="collapsible.html">Tu diseño</a></li>
+        <li><a class="" href="collapsible.html">Unete</a></li>
+        <li><a class="" href="collapsible.html">Contáctanos</a></li>
+        <li><a href="mobile.html"><i class="material-icons">shopping_cart</i></a></li>
+        <li><a class="waves-effect waves-light btn">Login</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a class="" href="sass.html"><b>Nosotros</b></a></li>
+        <li><a class="" href="src/jewelry.php">Joyeria</a></li>
+        <li><a class="" href="collapsible.html">Hogar</a></li>
+        <li><a class="" href="collapsible.html">Tu diseño</a></li>
+        <li><a class="" href="collapsible.html">Unete</a></li>
+        <li><a class="" href="collapsible.html">Contáctanos</a></li>
+        <li><a class="" href="collapsible.html">Carrito</a></li>
+        <li><a class="" href="collapsible.html">Login</a></li>
+      </ul>
+    </div>
+  </nav>
+
+<!---     Slider      -->
+<div class="slider" align="left">
+    <ul class="slides">
+      <li>
+        <img src="img/home/slider/heroimage@2xBornita.png"> <!-- random image -->
+        <div class="caption " align="left">
+          <h1>Joyeria <br>Artesanal</h1>
+        </div>
+      </li>
+      <li>
+        <img src="http://lorempixel.com/1230/547/nature/7"> <!-- random image -->
+        <div class="caption left-align">
+           <h1>Joyeria <br>Artesanal</h1>
+        </div>
+      </li>
+      <li>
+        <img src="http://lorempixel.com/1230/547/nature/10"> <!-- random image -->
+        <div class="caption left-align">
+           <h1>Joyeria <br>Artesanal</h1>
+        </div>
+      </li>
+    </ul>
+  </div>
+
+<!---     Novedades      -->
+<div class="">
+    <div class="">
+    <div align="center">
+        <h5 class="selected"><a >NOVEDADES</a></h5>    
+    </div>
+    <div class="grid">
+      <div class="grid-item">
+          <img class="responsive-img" src="img/home/grid/328_455.jpg" />
+      </div>
+      <div class="grid-item grid-item--height2">
+          <img class="responsive-img" src="img/home/grid/328_338.jpg" />
+      </div>
+      <div class="grid-item grid-item--height3">
+          <img class="responsive-img" src="img/home/grid/328_220.jpg" />
+      </div>
+      <div class="grid-item grid-item--height3">
+          <img class="responsive-img" src="img/home/grid/328_220.jpg" />
+      </div>
+       <div class="grid-item grid-item--height3">
+          <img class="responsive-img" src="img/home/grid/328_220.jpg" />
+      </div>
+      <div class="grid-item">
+          <img class="responsive-img" src="img/home/grid/328_455.jpg" />
+      </div>
+      <div class="grid-item grid-item--height2">
+          <img class="responsive-img" src="img/home/grid/328_338.jpg" />
+      </div>
+      <div class="grid-item grid-item--height3">
+          <img class="responsive-img" src="img/home/grid/328_220.jpg" />
+      </div>
+      <div class="grid-item grid-item--height3">
+          <img class="responsive-img" src="img/home/grid/328_220.jpg" />
+      </div>
+  
+      </div>
+
+    </div>
+</div>
+<br><br>
+<div class="" >
+    <div class="section light-blue lighten-2">
+
+        <!--   Icon Section   -->
+        <div align="center">
+            <h5 class="selected-white"><a >VALORES</a></h5>  
+        </div><br><br><br>
+        <div class="row">
+            <div class="col s12 m6">             
+                <div class="row">
+                    <div class="col s6 right-align white-text">
+                       <i class="fa fa-check-square-o fa-5x" ></i>
+                   </div>
+                   <div class="left-align col s6">
+                       <br>
+                       <h5 class=" white-text">CALIDAD</h5>
+                   </div>
+               </div>
+            </div>
+
+            <div class="col s12 m6">             
+                <div class="row">
+                    <div class="col s6 right-align white-text">
+                        <i class="fa fa-heart-o  fa-5x" ></i>
+                   </div>
+                   <div class="left-align col s6">
+                       <br>
+                       <h5 class=" white-text">PASION</h5>
+                   </div>
+               </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col s12 m6">             
+                <div class="row">
+                    <div class="col s6 right-align white-text">
+                       <i class="fa fa-handshake-o fa-5x" ></i>
+                   </div>
+                   <div class="left-align col s6">
+                       <br>
+                       <h5 class=" white-text">HONRADEZ</h5>
+                   </div>
+               </div>
+            </div>
+
+            <div class="col s12 m6">             
+                <div class="row">
+                    <div class="col s6 right-align white-text">
+                      <i class="fa fa-lightbulb-o fa-5x" ></i>
+                   </div>
+                   <div class="left-align col s6">
+                       <br>
+                       <h5 class=" white-text">CREATIVIDAD</h5>
+                   </div>
+               </div>
+            </div>
+        </div>
+        <br>
+    </div>
+</div>
+
+<!--   Comentarios   -->
+<div class="container">
+    <div class="section white">
+
+        <!--   Icon Section   -->
+        <div align="center">
+            <h5 class="selected"><a >OPINIONES</a></h5><br><br><br> 
+        </div>
+        <div class="row valign-wrapper">
+            <div class="col s2">
+              <img src="img/home/comentarios/mujer.jpeg" alt="" class="circle-border responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <img src="img/icono/“comilla izq@2x.png" width="50px" alt="">              
+                  <span class="black-text">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                  </span>
+                <img src="img/icono/“ comilla der@2x.png" width="50px" alt="">  
+              
+            </div>
+          </div>
+    </div>
+</div>
+
+<br>
+
+<div class="parallax-container valign-wrapper">
+    <div class="section no-pad-bot">
+        <div class="container">
+            <div class="row center">
+                <h2 class="header col s12 white-text">Crea tu propio diseño</h2>
+            </div>
+            <div class="row center">
+                <a class="btn-start">PERSONALIZA</a>
+            </div>
+        </div>
+    </div>
+    <div class="parallax"><img src="img/home/parallax/Santa clara del cobre@2x.png" alt="Unsplashed background img 2"></div>
+</div>
+
+<div>
+  <div align="center">
+    <h5 class="selected"><a >CONTACTO</a></h5><br><br><br> 
+  </div>
+  <div>
+      <div class="row">
+         <div class="col s12 m6">
+           <form action="" method="get" accept-charset="utf-8">
+             <div class="row">
+              <div class="input-field col s12">
+                <input id="last_name" type="text" class="validate">
+                <label for="last_name">NOMBRE *</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="last_name" type="text" class="validate">
+                <label for="last_name">CORREO *</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="last_name" type="text" class="validate">
+                <label for="last_name">TELEFONO</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <input id="last_name" type="text" class="validate">
+                <label for="last_name">ASUNTO *</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <textarea id="textarea1" class="materialize-textarea"></textarea>
+                <label for="textarea1">MENSAJE *</label>
+              </div>
+            </div>
+            <div class="row">
+              <div class="input-field col s12">
+                <a class="waves-effect  light-blue lighten-3 btn">ENVIAR</a>
+              </div>
+            </div>
+           </form>
+         </div>
+         <div class="col s12 m6">
+           <img src="img/home/contacto/contactoimage@2x.png" class="responsive-img" alt="">
+         </div>
+      </div>
+  </div>
+</div>
+<div class="row">
+    <div class="col s12">
+        <div id="google-map"></div>
+         <script>
+      var map;
+      function initMap() {
+        map = new google.maps.Map(document.getElementById('google-map'), {
+          center: {lat: -34.397, lng: 150.644},
+          zoom: 8
+        });
+      }
+    </script>
+    </div>
+</div>
+<footer class="page-footer grey lighten-4">
+    <div class="container">
+        <div class="row">
+            <div class="col l3 s12">
+                <h6 class="teal-text lighten-2-text">AYUDA</h6>
+                <ul>
+                    <li><a class="grey-text darken-4-text" href="#!">Terminos de uso</a></li>
+                    <li><a class="grey-text darken-4-text" href="#!">Preguntas frecuentes</a></li>
+                    <li><a class="grey-text darken-4-text" href="#!">¿QUIERES SER PROMOTOR?</a></li>
+                    
+                </ul>
+            </div>
+            <div class="col l3 s12">
+                <h6 class="teal-text lighten-2-text">CONTACTO</h6>
+                <ul>
+                    <li><a class="black-text" >Domicilio: </a><a class="grey-text darken-4-text">Centro #268</a></li>
+                    <li><a class="black-text" >Telefono: </a><a class="grey-text darken-4-text">4433231867</a></li>
+                    <li><a class="black-text" >Correo: </a><a class="grey-text darken-4-text">ventas@bornitac.com</a></li>
+                </ul>
+            </div>
+            <div class="col l3 s12">
+                <h6 class="teal-text lighten-2-text">QUIENES SOMOS</h6>
+                <ul>
+                    <li><a class="grey-text darken-4-text" href="#!">Somos</a></li>
+                </ul>
+            </div>
+            <div class="col l3 s12">
+                <ul>
+                    <li><img src="img/icono/Bfacebook@2x.png" width="35px" alt="">&nbsp; &nbsp;<img src="img/icono/Btwitter@2x.png" width="35px" alt="">&nbsp; &nbsp;<img src="img/icono/Binstagram@2x.png" width="35px" alt=""></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container" align="center">
+            <a class="brown-text text-lighten-3" >&copy; Copyright 2017. Todos los derechos reservados</a>
+        </div>
+    </div>
+</footer>
+
+
+<!--  Scripts-->
+ 
+
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="materialize/js/materialize.js"></script>
+<script src="materialize/js/init.js"></script>
+<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB5Sjy68hWY2bsARn-jOE_uhGeYy7OYnxo&callback=initMap"
+  type="text/javascript"></script>
+<script src="materialize/js/main.js"></script>
+<script src="materialize/js/masonry.pkgd.js"></script>
+
+<!-- <script type="text/javascript" src="js/jquery.js"></script>
+<script type="text/javascript" src="js/jquery.inview.min.js"></script> -->
+<!-- <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script> -->
+
+</body>
+</html>
---
+++ src/jewelry.php
@@ -0,0 +1,98 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0, user-scalable=no"/>
+    <title>Disfruta comprando en Bornita</title>
+
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.0.0-beta.3/assets/owl.carousel.min.css'>
+    <link rel='stylesheet prefetch' href='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.0.0-beta.3/assets/owl.theme.default.min.css'>
+    <link rel="stylesheet" href="../materialize/css/style1.css">
+    <link href="../materialize/css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="../materialize/css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="../materialize/css/main.css" type="text/css" rel="stylesheet" />
+    <link rel="stylesheet" href="../materialize/css/font-awesome-4.7.0/css/font-awesome.min.css">
+
+</head>
+<body>
+
+<!--          Menu         -->
+<nav class="nav-extended white top-menu" id="top-menu">
+    <div class="nav-wrapper " >
+      <a href="#" class="brand-logo">Logo</a>
+      <a href="#" data-activates="mobile-demo" class="button-collapse"><i class="material-icons">menu</i></a>
+      <ul id="nav-mobile" class="right hide-on-med-and-down">
+        <li><a class="" href="sass.html">Nosotros</a></li>
+        <li class="selected"><a class="" href="jewelry.php">Joyeria</a></li>
+        <li><a class="" href="collapsible.html">Hogar</a></li>
+        <li><a class="" href="collapsible.html">Tu diseño</a></li>
+        <li><a class="" href="collapsible.html">Unete</a></li>
+        <li><a class="" href="collapsible.html">Contáctanos</a></li>
+        <li><a href="mobile.html"><i class="material-icons">shopping_cart</i></a></li>
+        <li><a class="waves-effect waves-light btn">Login</a></li>
+      </ul>
+      <ul class="side-nav" id="mobile-demo">
+        <li><a class="" href="sass.html"><b>Nosotros</b></a></li>
+        <li><a class="" href="jewelry.php">Joyeria</a></li>
+        <li><a class="" href="collapsible.html">Hogar</a></li>
+        <li><a class="" href="collapsible.html">Tu diseño</a></li>
+        <li><a class="" href="collapsible.html">Unete</a></li>
+        <li><a class="" href="collapsible.html">Contáctanos</a></li>
+        <li><a class="" href="collapsible.html">Carrito</a></li>
+        <li><a class="" href="collapsible.html">Login</a></li>
+      </ul>
+    </div>
+  </nav>
+
+<!-- <div class="row">
+  <div class="responsive-img col s8 m8 pull-m4" style="position:fixed !important">
+    
+  </div>
+  <div class="col s12 offset-s6 push-s4" style="position:fixed !important">
+    <h2 class="black-text">Ejemplo</h2>
+  </div>
+</div> -->
+<div class="row">
+      <div class="col s4 push-s8">
+          <h2 class="black-text">Ejemplo</h2>
+      </div>
+      <div class="col s8 pull-s4">
+        <div id="sync1" class="owl-carousel owl-theme">
+          <div class="item">
+            <img src="../img/home/slider/heroimage@2xBornita.png"> <!-- random image -->
+          </div>
+          <div class="item">
+            <img src="http://lorempixel.com/1230/547/nature/7"> <!-- random image -->
+          </div>
+          <div class="item">
+            <img src="http://lorempixel.com/1230/547/nature/10"> <!-- random image -->
+          </div>
+        </div>
+
+        <div id="sync2" class="owl-carousel owl-theme">
+          <div class="item">
+            <img src="../img/home/slider/heroimage@2xBornita.png"> <!-- random image -->
+          </div>
+          <div class="item">
+            <img src="../img/home/slider/heroimage@2xBornita.png"> <!-- random image -->
+          </div>
+          <div class="item">
+            <img src="../img/home/slider/heroimage@2xBornita.png"> <!-- random image -->
+          </div>
+        </div>
+      </div>
+</div>
+  <!--  Scripts-->
+  <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+  <script src='https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.0.0-beta.3/owl.carousel.min.js'></script>
+  <script src="../materialize/js/index.js"></script>
+  <script src="../materialize/js/materialize.js"></script>
+  <script src="../materialize/js/init.js"></script>
+  <script src="../materialize/js/main.js"></script>
+  <script src="../materialize/js/masonry.pkgd.js"></script>
+  <script type="text/javascript" src="http://maps.google.com/maps/api/js?sensor=true"></script>
+</body>
+</html>
